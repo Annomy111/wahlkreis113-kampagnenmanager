6a68811a891f75b8cfa75a2201dc93844acf4d8f
Annomy111 <Fedo.hagge-kubat@fu-berlin.de>
2024-11-30 23:08:24 +0100

    Update App.js and package.json, add store directory

--- client/src/App.js
+++ client/src/App.js
@@ -1,52 +1,15 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { ThemeProvider, createTheme } from '@material-ui/core/styles';
-import CssBaseline from '@material-ui/core/CssBaseline';
-
-// Layout Components
-import Navbar from './components/layout/Navbar';
-import Sidebar from './components/layout/Sidebar';
-
-// Page Components
-import Dashboard from './pages/Dashboard';
-import Login from './pages/Login';
-import Tasks from './pages/Tasks';
-import Calendar from './pages/Calendar';
-import DoorToDoor from './pages/DoorToDoor';
-
-const theme = createTheme({
-  palette: {
-    primary: {
-      main: '#E3000F', // SPD Red
-    },
-    secondary: {
-      main: '#000000',
-    },
-  },
-});
+import { Box, Container, Typography } from '@mui/material';
 
 function App() {
   return (
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <Router>
-        <div className="app">
-          <Navbar />
-          <div className="container">
-            <Sidebar />
-            <main className="main-content">
-              <Routes>
-                <Route path="/" element={<Dashboard />} />
-                <Route path="/login" element={<Login />} />
-                <Route path="/tasks" element={<Tasks />} />
-                <Route path="/calendar" element={<Calendar />} />
-                <Route path="/door-to-door" element={<DoorToDoor />} />
-              </Routes>
-            </main>
-          </div>
-        </div>
-      </Router>
-    </ThemeProvider>
+    <Container maxWidth="lg">
+      <Box sx={{ my: 4 }}>
+        <Typography variant="h4" component="h1" gutterBottom>
+          Wahlkreis 113 Kampagnenmanager
+        </Typography>
+      </Box>
+    </Container>
   );
 }
 
